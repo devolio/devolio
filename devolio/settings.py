5d451e3cb404fcb2ef5d6c9278d3202f93ab33be
Mustafa Abdelhai <hi@mustafa.li>
2017-03-10 23:32:46 +0100

    configured static files

--- devolio/settings.py
+++ devolio/settings.py
@@ -139,6 +139,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Site ID, added for 'django-allauth'
