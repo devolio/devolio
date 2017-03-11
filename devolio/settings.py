f5b006fdbd082eb866ad9fcda89b8de804d29330
Mustafa Abdelhai <hi@mustafa.li>
2017-03-12 00:36:45 +0100

    updated form styles

--- devolio/settings.py
+++ devolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJ_DEBUG', True)
 
-ALLOWED_HOSTS = ['next.devolio.net', 'devolioapp.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['next.devolio.net', 'devolioapp.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
