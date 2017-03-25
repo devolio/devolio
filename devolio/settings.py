ff101871a95d9e1ae94b39b1325542f7bb363d44
Mustafa Abdelhai <hi@mustafa.li>
2017-03-25 23:12:51 +0100

    update settings

--- devolio/settings.py
+++ devolio/settings.py
@@ -151,9 +151,9 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_HOST = os.environ.get('DJANGO_STATIC_HOST', '')
