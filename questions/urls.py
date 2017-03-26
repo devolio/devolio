ef0a699c8f7a4aabde703a5fd4e9b4e322536dbe
Mustafa Abdelhai <hi@mustafa.li>
2017-03-26 14:34:52 +0200

    added tags questions

--- questions/urls.py
+++ questions/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     url(r'^ask$', qv.QuestionCreateView.as_view(), name="ask"),
     url(r'^questions$', qv.questions_list, name="questions"),
     url(r'^response$', qv.create_response, name="response"),
-    url(r'^tags/(?P<slug>[-\w]+)$', qv.TagQuestionsList.as_view(), name="tags_tag"),
-    url(r'^tags/$', qv.TagListView.as_view()),
+    url(r'^tags/(?P<slug>[-\w]+)$', qv.tag_questions_list, name="tags_tag"),
     url(r'^(?P<slug>[-\w]+)/edit$', qv.QuestionUpdateView.as_view(), name="q_edit"),
     url(r'^(?P<slug>[-\w]+)$', qv.QuestionDetailView.as_view(), name="q_detail")
 
