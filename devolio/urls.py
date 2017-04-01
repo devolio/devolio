f76883b1629197636abd6d6fda497f25f90e02a6
Mustafa Abdelhai <hi@mustafa.li>
2017-04-01 21:00:40 +0200

    update tags urls

--- devolio/urls.py
+++ devolio/urls.py
@@ -9,10 +9,12 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^users/', include('allauth.urls')),
     url(r'^dashboard', uv.dashboard, name='dashboard'),
-    url(r'^profile/create', uv.ProfileCreateView.as_view(), name='create_profile'),
-    url(r'^@(?P<slug>[-\w]+)/update', uv.ProfileUpdateView.as_view(), name='update_profile'),
+    url(r'^profile/create', uv.ProfileCreateView.as_view(),
+        name='create_profile'),
+    url(r'^@(?P<slug>[-\w]+)/update', uv.ProfileUpdateView.as_view(),
+        name='update_profile'),
     url(r'^@(?P<slug>[-\w]+)', uv.public_profile, name='public_profile'),
-    url(r'^tags/(?P<slug>[-\w]+)$', qv.tag_questions_list, name="tags_tag"),
+    url(r'^t/(?P<slug>[-\w]+)$', qv.tag_questions_list, name="tags_tag"),
     url(r'^q/', include('questions.urls')),
     url(r'^$', wv.index, name='ws_index')
 ]
